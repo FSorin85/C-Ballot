e05bf44ddf652453f85808b2990e57a185d6384d
FSorin85 <florian.sorin85@gmail.com>
2018-06-11 13:32:22 +0200

    modif Inscription css et js

--- Inscription.js
+++ Inscription.js
@@ -1,16 +1,16 @@
-// var password = document.getElementById("passwd")
-//     , confirm_password = document.getElementById("passwdVerif");
-//
-// function validatePassword(){
-//     if(password.value != confirm_password.value) {
-//         confirm_password.setCustomValidity("Les mots de passe ne correspondent pas");
-//     } else {
-//         confirm_password.setCustomValidity('');
-//     }
-// }
-//
-// password.onchange = validatePassword;
-// confirm_password.onkeyup = validatePassword;
+var password = document.getElementById("passwd")
+    , confirm_password = document.getElementById("passwdVerif");
+
+function validatePassword(){
+    if(password.value != confirm_password.value) {
+        confirm_password.setCustomValidity("Les mots de passe ne correspondent pas");
+    } else {
+        confirm_password.setCustomValidity('');
+    }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
 
 var email = document.getElementById("email")
     , verif_email = document.getElementById("emailVerif");
