bf8c0bdc60867bbac57e74d4b3f402b2794ac3e8
FSorin85 <florian.sorin85@gmail.com>
2018-06-12 13:53:02 +0200

    correction d'une erreur d'auto incrémentation

--- CoBDD.php
+++ CoBDD.php
@@ -1,20 +1,24 @@
 <?php
 
+try {
+    @$firstname = $_POST['firstname'];
+    @$lastname = $_POST['lastname'];
+    @$password = password_hash($_POST['passwd'], PASSWORD_DEFAULT);
+    @$email = $_POST['email'];
 
-@$firstname = $_POST['firstname'];
-@$lastname = $_POST['lastname'];
-@$password = password_hash($_POST['passwd'], PASSWORD_DEFAULT);
-@$email = $_POST['email'];
+    $connection = new PDO("mysql:host=localhost;dbname=cballot", 'root', '');
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$connection = new PDO("mysql:host=localhost;dbname=cballot", 'root', '');
-$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-$query = 'INSERT INTO cballot.user (firstname, lastname, password, email) VALUES (:firstname,:lastname, :password, :email)';
-$stmt = $connection->prepare($query);
-$stmt->bindValue('firstname', $firstname);
-$stmt->bindValue('lastname', $lastname);
-$stmt->bindValue('password', $password);
-$stmt->bindValue('email', $email);
-$stmt->execute();
-header('Location: http://localhost/cballot/Accueil.php');
+    $query = 'INSERT INTO cballot.user (firstname, lastname, password, email) VALUES (:firstname,:lastname, :password, :email)';
+    $stmt = $connection->prepare($query);
+    $stmt->bindValue('firstname', $firstname);
+    $stmt->bindValue('lastname', $lastname);
+    $stmt->bindValue('password', $password);
+    $stmt->bindValue('email', $email);
+    $stmt->execute();
+    header('Location: http://localhost/cballot/Accueil.php');
+}
+catch (Exception $e) {
+    echo 'Exception reçue : ',  $e->getMessage(), "\n";
+}
 exit();
--- cballot.sql
+++ cballot.sql
@@ -3,16 +3,16 @@ CREATE DATABASE IF NOT EXISTS cballot;
 use cballot
 
 CREATE TABLE user (
-  iduser BIGINT  NOT NULL ,
+  iduser BIGINT  NOT NULL AUTO_INCREMENT,
   firstname VARCHAR(30)  NOT NULL ,
   lastname VARCHAR(30)  NOT NULL ,
-  password VARCHAR(30)  NOT NULL ,
+  password VARCHAR(128)  NOT NULL ,
   email VARCHAR(30)  NOT NULL ,
   PRIMARY KEY (iduser)
 );
 
 CREATE TABLE voter (
-  idvoter BIGINT NOT NULL,
+  idvoter BIGINT NOT NULL AUTO_INCREMENT,
   emailvoter VARCHAR(30) NOT NULL,
   PRIMARY KEY (idvoter),
   FOREIGN KEY (iduser) REFERENCES user (iduser)
