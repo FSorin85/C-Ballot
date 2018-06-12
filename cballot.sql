dc697b1d61239b9bc3743cd3413bbfaf66888a1a
FSorin85 <florian.sorin85@gmail.com>
2018-06-12 11:22:45 +0200

    modification du script sql

--- cballot.sql
+++ cballot.sql
@@ -14,6 +14,6 @@ CREATE TABLE user (
 CREATE TABLE voter (
   idvoter BIGINT NOT NULL,
   emailvoter VARCHAR(30) NOT NULL,
-  iduser BIGINT NOT NULL,
-  PRIMARY KEY (idvoter)
+  PRIMARY KEY (idvoter),
+  FOREIGN KEY (iduser) REFERENCES user (iduser)
 );
