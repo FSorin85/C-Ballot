d47da619dd8fcd35d40057256b6f8c62bcdd591b
FSorin85 <florian.sorin85@gmail.com>
2018-06-12 11:24:42 +0200

    ajout de la page d'accueil

---
+++ Accueil.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CBallot</title>
+
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="Accueil.css" type="text/css"/>
+</head>
+<body>
+<h1>Bienvenue</h1>
+
+</body>
+</html>
