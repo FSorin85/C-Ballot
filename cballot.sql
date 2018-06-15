4cdf734915e2706be1878bbcc24639047b53ae26
FSorin85 <florian.sorin85@gmail.com>
2018-06-15 10:18:34 +0200

    MAJ CSS

--- cballot.sql
+++ cballot.sql
@@ -8,7 +8,8 @@ CREATE TABLE user (
   lastname VARCHAR(30)  NOT NULL ,
   password VARCHAR(128)  NOT NULL ,
   email VARCHAR(30)  NOT NULL ,
-  PRIMARY KEY (iduser)
+  PRIMARY KEY (iduser),
+  CONSTRAINT uc_person_email UNIQUE (email)
 );
 
 CREATE TABLE voter (
@@ -17,4 +18,23 @@ CREATE TABLE voter (
   iduser BIGINT NOT NULL,
   PRIMARY KEY (idvoter),
   FOREIGN KEY (iduser) REFERENCES user(iduser)
+);
+
+CREATE TABLE sondage (
+  idsondage BIGINT NOT NULL AUTO_INCREMENT,
+  questionsondage VARCHAR(128) NOT NULL,
+  choix1sondage VARCHAR(128) NOT NULL,
+  choix2sondage VARCHAR(128) NOT NULL,
+  choix3sondage VARCHAR(128) NOT NULL,
+  iduser BIGINT  NOT NULL,
+  PRIMARY KEY (idsondage),
+  FOREIGN KEY (iduser) REFERENCES user(iduser)
+);
+
+CREATE TABLE retention (
+  idretention BIGINT AUTO_INCREMENT NOT NULL,
+  email VARCHAR(30)  NOT NULL,
+  firstname VARCHAR(30)  NOT NULL,
+  lastname VARCHAR(30)  NOT NULL,
+  PRIMARY KEY (idretention)
 );
